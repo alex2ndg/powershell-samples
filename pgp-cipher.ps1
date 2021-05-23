afa3f08ee45ecaed9a6bc283f5f54b098d615be5
Alejandro González García <74770403+alex2ndg@users.noreply.github.com>
2021-05-23 20:25:19 +0200

    Creation of pgp-cipher.ps1
    This script will allow you to cipher via PGP any specific file, using the defined already installed keys.

---
+++ pgp-cipher.ps1
@@ -0,0 +1,15 @@
+# This script will allow you to cipher via PGP any specific file, using the defined already installed keys.
+# It'll need the original directory of the file, original file name, PGP key and output of the file.
+
+param([string]$dir = "www", [string]$ofile = "xxx", [string]$key = "yyy", [string]$ffile = "zzz")
+
+Write-Host "Directory: $dir"
+Write-Host "Original File: $ofile"
+Write-Host "Key: $key"
+Write-Host "Final File: $ffile"
+
+# You'll have to define the --home-dir just in case if the user you are using doesn't has the keyring defined properly
+# Passphrase is optional - on this sample we are using a blank space.
+cd $dir
+pgp --home-dir "C:\Users\my_user\PGP" -e $ofile -r $key -s --output $ffile --passphrase " "
+Remove-Item $entrada
